68462a8ce2ec22d96562e2ffd179308a4eacda82
kashim rahman mahdi <mahdi.rahman356@gmail.com>
2025-11-02 20:09:03 +0600

    complete doctor create, update, delete, view system for admin

---
+++ src/app/(dashboardLayout)/admin/dashboard/manage-doctors/page.tsx
@@ -0,0 +1,11 @@
+import { DoctorTable } from "@/components/modules/Dashboard/Dashboard/ManageDoctor/DoctorTable";
+
+const manageDoctorPage = () => {
+    return (
+        <div>
+            <DoctorTable />
+        </div>
+    );
+};
+
+export default manageDoctorPage;
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 export interface IUser {
     id: string,
@@ -7,9 +8,41 @@ export interface IUser {
     iat: number
 }
 
-
-
 export interface IUserContext {
   user: IUser | null;
   setUser: React.Dispatch<React.SetStateAction<IUser | null>>;
+}
+
+export interface IDoctor {
+  id: string
+  name: string
+  email: string
+  profilePhoto: string
+  contactNumber: string
+  address: string
+  registrationNumber: string
+  experience: number
+  gender: string
+  appointmentFee: number
+  qualification: string
+  currentWorkingPlace: string
+  designation: string
+  averageRating: number
+  isDeleted: boolean
+  createdAt: string
+  updatedAt: string
+  doctorSpecialties: DoctorSpecialty[]
+  reviews: any[]
+}
+
+export interface DoctorSpecialty {
+  specialitiesId: string
+  doctorId: string
+  specialities: Specialities
+}
+
+export interface Specialities {
+  id: string
+  title: string
+  icon: string
 }
